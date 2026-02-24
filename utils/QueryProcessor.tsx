50cfc95dbba6bcffc76f6e60b7dff247e2d16045
bendnema <bnematad@andrew.cmu.edu>
2026-02-24 10:34:05 -0500

    Multiplication and add method

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -15,6 +15,17 @@ export default function QueryProcessor(query: string): string {
     return "bnematad";
   }
 
+  // 	What is 91 multiplied by 12 plus 17?
+  if (query.toLowerCase().includes("multiplied by") && query.toLowerCase().includes("plus")) {
+    const numbers = query.match(/\d+/g);
+    if (numbers && numbers.length >= 3) {
+      const first = Number(numbers[0]);
+      const second = Number(numbers[1]);
+      const third = Number(numbers[2]);
+      return (first * second + third).toString();
+    }
+  }
+
   // What is 97 to the power of 34?
   if (query.toLowerCase().includes("to the power of")) {
     const numbers = query.match(/\d+/g);
